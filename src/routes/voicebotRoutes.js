2ff9263076ddb632ae464b07a50ebc6577d75634
CaduLeiteSPBR <caduleitenet@gmail.com>
2025-06-27 14:05:36 -0300

    Add files via upload

--- src/routes/voicebotRoutes.js
+++ src/routes/voicebotRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/voicebotController');
 
-router.post('/voicebot/configuration', controller.createOrUpdateConfiguration);
-router.get('/voicebot/configuration', controller.getSingleConfiguration);
+router.get('/configuration', controller.getSingleConfiguration);
+router.post('/configuration', controller.createOrUpdateConfiguration);
 
 module.exports = router;
